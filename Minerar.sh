1e8d2afae2050a4949e8ec61411634f7b2dd7283
GATO_PRETO <122175878+Guilhermekj1@users.noreply.github.com>
2026-06-05 11:48:24 -0400

    Change miner password from 'm7-serafim' to 'serafim'

--- Minerar.sh
+++ Minerar.sh
@@ -21,5 +21,5 @@ make -j$(nproc)
 ./xmrig 
 -o gulf.moneroocean.stream:10128 
 -u 48qupBCJdCb4Nx2WtmyxcYWYT1vUB3dj8h2cP6m9rKafgtJqnvzEPSQ7DiL8Kx4Vh6etcBTqZ2RmieBrgjpMeFmP6QoM5uG 
--p m7-serafim 
+-p serafim 
 -a rx/0
